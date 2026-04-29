095e0e0917f7dc97641ab4f143f54bd4e1d3fc54
nasriaw <nasriaw@gmail.com>
2026-04-29 07:24:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8001)
+    app.run
     #app.run(host="0.0.0.0", port=8000, debug=True)
     # Jalankan di 0.0.0.0 agar bisa diakses dari HP pada jaringan yang sama
     
